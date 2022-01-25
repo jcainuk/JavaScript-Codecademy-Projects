bf2bfef3b89349006489818a8f909dd246327cfc
Jonathan Cain <jcainuk@gmail.com>
2022-01-25 14:00:07 +0000

    log string interpolation of temp result

--- Kelvin Weather/app.js
+++ Kelvin Weather/app.js
@@ -6,3 +6,5 @@ const celsius = kelvin - 273;
 
 // Conversion of celsius to fahrenheit and round down result
 const fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
