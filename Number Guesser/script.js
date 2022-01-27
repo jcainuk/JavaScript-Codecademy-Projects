eb9820292e57c8ffa12e1cbf9531c2efc8f804ec
Jonathan Cain <jcainuk@gmail.com>
2022-01-27 16:29:24 +0000

    create generateTarget function

--- Number Guesser/script.js
+++ Number Guesser/script.js
@@ -1,6 +1,6 @@
-let humanScore = 0;
-let computerScore = 0;
-let currentRoundNumber = 1;
+const humanScore = 0;
+const computerScore = 0;
+const currentRoundNumber = 1;
 
 // Write your code below:
-
+const generateTarget = () => Math.floor(Math.random() * 10);
