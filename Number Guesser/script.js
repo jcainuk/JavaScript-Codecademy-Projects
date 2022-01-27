118fcee01b944b2e21d3e3095cd9ee718788bb71
Jonathan Cain <jcainuk@gmail.com>
2022-01-27 16:31:02 +0000

    comment for generateTarget function

--- Number Guesser/script.js
+++ Number Guesser/script.js
@@ -3,4 +3,6 @@ const computerScore = 0;
 const currentRoundNumber = 1;
 
 // Write your code below:
+
+// return a random integer between 0 and 9
 const generateTarget = () => Math.floor(Math.random() * 10);
