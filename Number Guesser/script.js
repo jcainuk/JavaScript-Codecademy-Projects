70797839ba6a62c627931ffefd82223c7de283bc
Jonathan Cain <jcainuk@gmail.com>
2022-01-27 16:47:57 +0000

    create compareGuesses function

--- Number Guesser/script.js
+++ Number Guesser/script.js
@@ -6,3 +6,19 @@ const currentRoundNumber = 1;
 
 // return a random integer between 0 and 9
 const generateTarget = () => Math.floor(Math.random() * 10);
+
+// determine which guess is closest to target each round
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+  if (humanGuess === computerGuess) {
+    return true;
+  }
+
+  const difference = (a, b) => Math.abs(a - b);
+
+  const computerDiffFromTarget = difference(targetGuess, computerGuess);
+
+  const humanDiffFromTarget = difference(targetGuess, humanGuess);
+
+  return computerDiffFromTarget > humanDiffFromTarget;
+};
