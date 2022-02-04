c0a2ae7344777d7223fdce437eb96da32da0f57a
Jonathan Cain <jcainuk@gmail.com>
2022-02-04 15:18:13 +0000

    create clamp method

--- lodash/_.js
+++ lodash/_.js
@@ -1,5 +1,11 @@
 const _ = {
+  clamp(number, lower, upper) {
+    const lowerClampedValue = Math.max(number, lower);
 
+    const clampedValue = Math.min(lowerClampedValue, upper);
+
+    return clampedValue;
+  },
 };
 
 // Do not write or modify code below this line.
