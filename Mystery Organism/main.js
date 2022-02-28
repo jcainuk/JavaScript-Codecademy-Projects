be10e510b22024a035f3202833d898d26a858e20
Jonathan Cain <jcainuk@gmail.com>
2022-02-28 12:43:03 +0000

    create pAequorFactory function for organism with mutate method

--- Mystery Organism/main.js
+++ Mystery Organism/main.js
@@ -13,10 +13,32 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+// factory function
+const pAequorFactory = (specimenNum, DNAarray) => ({
+  specimenNum,
+  DNAarray,
+  mutate() {
+  // generate random base
+    const base = returnRandBase();
 
+    // select random index from DNAarray
+    const randomIndex = Math.floor(Math.random() * DNAarray.length);
 
+    // change base at index
+    if (DNAarray[randomIndex] !== base) {
+      DNAarray[randomIndex] = base;
+    } else {
+      this.mutate();
+    }
+  },
 
+});
 
+// test factory function
 
+const newCreature = pAequorFactory(1, mockUpStrand());
 
+console.log(newCreature);
 
+newCreature.mutate();
+console.log(newCreature.DNAarray);
