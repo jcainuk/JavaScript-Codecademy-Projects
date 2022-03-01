4c01dad11cbf0a01f1d212508a1822990355b5cb
Jonathan Cain <jcainuk@gmail.com>
2022-03-01 16:14:25 +0000

    wrote adverbs of frequency

---
+++ Mixed Messages/app.js
@@ -0,0 +1,12 @@
+// Random inspirational quote generator
+
+// adverbs of frequency array
+
+const adverbOfFrequency = [
+  'always',
+  'usually',
+  'often',
+  'sometimes',
+  'rarely',
+  'never',
+];
