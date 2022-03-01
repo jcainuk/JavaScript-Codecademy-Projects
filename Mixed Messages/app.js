e67a69e773f97f8012c95069895ba18a5da816e8
Jonathan Cain <jcainuk@gmail.com>
2022-03-01 16:40:02 +0000

    write phrases with the verb set array and fixed pseudocode

--- Mixed Messages/app.js
+++ Mixed Messages/app.js
@@ -51,6 +51,28 @@ const adjectives = [
   'young',
 ];
 
+// Phrase with the verb set
+const phrasesWithTheVerbSet = [
+  'a ladder against the wall',
+  'a stone on the grave',
+  'a trap ready to catch prey',
+  'aside some money for a rainy day',
+  'another goal',
+  'the house on fire',
+  'the door ajar',
+  'a wedding day',
+  'the rules for the game',
+  'a record for the half mile',
+  'an example of generosity',
+  'lessons for the children to work upon at home',
+  'a thermostat at 68',
+  'a broken bone',
+  'sail for America',
+  'the table for dinner',
+  'your first novel in Paris',
+  'foot in a minefield',
+];
+
 // example quote
-/* “You are never too old to set another goal
+/* “You are ADVERBOFFREQUENCY too ADJECTIVE to set PHRASEWITHSET
  or to dream a new dream.” */
