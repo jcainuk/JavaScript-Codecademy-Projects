6a6829b117b7bd5ffe172854e0010a3bb539e9b2
Jonathan Cain <jcainuk@gmail.com>
2022-03-14 19:27:20 +0000

    add formula for celsius to kelvin

--- Kelvin Weather/app.js
+++ Kelvin Weather/app.js
@@ -31,7 +31,7 @@ for (let i = 0; i < inputs.length; i++) {
 
       case 'celsius':
         // calculation of other values
-        // kelvinInput.value =
+        kelvinInput.value = value + 273.15;
         fahrenheitInput.value = Math.floor(value * (9 / 5) + 32);
         newtonInput.value = Math.floor(value * (33 / 100));
         break;
