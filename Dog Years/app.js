4ccd56881a809ca317589ced18afa6b2a37bc04f
Jonathan Cain <jcainuk@gmail.com>
2022-03-17 14:55:06 +0000

    refactor humanToDogYears function

--- Dog Years/app.js
+++ Dog Years/app.js
@@ -4,32 +4,22 @@ console.log(formElements);
 const humanInput = document.getElementById('human-age');
 const dogInput = document.getElementById('dog-age');
 
-const humanToDogYears = () => {};
-// Age variable
-const myAge = 33;
+const humanToDogYears = (age) => {
+  if (age < 0 || age === 0) { return 0; } if (age > 0) {
+  // The first 2 years of a dog's life
+    let earlyYears = 2;
+    earlyYears *= 10.5;
 
-// The first 2 years of a dog's life
-let earlyYears = 2;
-earlyYears *= 10.5;
+    // Account for first 2 years
+    let laterYears = age - 2;
 
-// Account for first 2 years
-let laterYears = myAge - 2;
+    // Number of dog years for later human years
+    laterYears *= 4;
 
-// Number of dog years for later human years
-laterYears *= 4;
-
-// check work
-console.log(earlyYears);
-console.log(laterYears);
-
-// Calculate my age in dog years
-const myAgeInDogYears = earlyYears + laterYears;
-
-// Store my name in lowercase letters
-const myName = 'Jonathan'.toLowerCase();
-
-//  Sentence to show your name and age in dog years
-console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+    // Calculate my age in dog years
+    return earlyYears + laterYears;
+  }
+};
 
 // for loop
 for (let i = 0; i < formElements.length; i++) {
@@ -40,7 +30,7 @@ for (let i = 0; i < formElements.length; i++) {
 
     switch (event.target.name) {
       case 'human-age':
-        // dogInput.value = ;
+        dogInput.value = humanToDogYears(value);
         break;
 
       case 'dog-age':
